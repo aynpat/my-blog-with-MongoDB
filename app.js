411cee7982f02f5b5fb16ad6d256291954d93712
aynpat <aynpat21@gmail.com>
2022-10-19 15:14:58 +0800

    added post page for individual blog post

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const mongoose = require("mongoose");
 let fs = require("fs")
 const path = require("path")
 
-const multer = require("multer")
+const multer = require("multer");
+const { ObjectId } = require("mongodb");
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
@@ -26,7 +27,8 @@ mongoose.connect('mongodb://127.0.0.1:27017/postDB')
 
 const postSchemas = new mongoose.Schema({
   title : String,
-  posts : String
+  posts : String,
+  
 })
 
 const postSchema = new mongoose.Schema({
@@ -35,7 +37,8 @@ const postSchema = new mongoose.Schema({
   img : {
     data : Buffer,
     contentType : String
-  }
+  },
+  date: { type: Date, required: true }
 });
 
 const blogPost = new mongoose.model('blogPost',postSchema);
@@ -62,7 +65,7 @@ app.get('/',(req,res) =>{
         res.status(500).send('An error occured', err)
       }
       else {
-        res.render('home', {items: items, timePost : today.toLocaleDateString("en-US", options)});
+        res.render('home', {items: items });
       }
     });
 });
@@ -75,7 +78,8 @@ app.post('/compose',upload.single('image'),(req, res) => {
      post : req.body.postMessage,
      img : {  data : fs.readFileSync(path.join(__dirname + '/uploads/' + req.file.filename)),
       contentType : 'image/png'
-    }
+    },
+    date : today.toLocaleDateString("en-US", options)
   
     
   }
@@ -135,7 +139,28 @@ app.get('/compose',(req,res)=>{
 //   res.redirect('/compose')
 // })
 
+////////////////////////////////////////////////////////////////
 
+app.get('/posts/:postId',(req,res)=>{
+  const postId = req.params.postId;
+  blogPost.findOne({_id : ObjectId(postId)},function(err,post){
+
+    if(!err){
+      console.log('walang error')
+      res.render('post',{
+        postTitle : post.title,
+        postContent : post.post
+      })
+    }
+    else{
+      console.log('may error')
+    }
+
+  })
+
+})
+
+//////////////////////////////////////////////////////////////
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
